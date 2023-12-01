79d0b8573e26fa350f5d58b7e2ec5f164a2679f6
delusional51 <BenjaminZanelli51@gmail.com>
2023-12-01 12:56:18 -0500

    turns name to index meow

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,10 +1,18 @@
+DOMSelectors ={
+    form: document.querySelector("#form"),
+    itemname: document.querySelector("#item-name"),
 
-const URL = `https://www.dnd5eapi.co/api/spells`;
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        response.foreach((el)=>console.log(el.index))}
-    catch(error){}
 }
-getData(URL)
+function test(){
+    let thing = DOMSelectors.itemname.value;
+    thing = thing.replaceAll(' ','-')
+    const URL = `https://www.dnd5eapi.co/api/spells/${thing}`; 
+    console.log(URL)
+}
+
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    test()
+});
+
 
