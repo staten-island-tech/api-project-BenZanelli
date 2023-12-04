a7af23582b377833cda52ebe43e1123ac68bb83f
Ben Zanelli <BenjaminZanelli51@gmail.com>
2023-12-04 12:38:47 -0500

    now work

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -7,6 +7,7 @@ async function getData(){
     let thing = DOMSelectors.itemname.value.toLowerCase();
     thing = thing.replaceAll(' ','-')
     let URL = `https://www.dnd5eapi.co/api/spells/${thing}`; 
+    console.log(URL)
     try {
         const response = await fetch(URL);
         console.log(response);
@@ -14,13 +15,25 @@ async function getData(){
         throw new Error(response.statusText);
     }
     const data = await response.json();
+    console.log(data.name)
     
+    DOMSelectors.parent.insertAdjacentHTML(
+        "beforeend",
+        `<div class='card'>
+        <h2 id="name" class="name">${data.name}</h2>
+        <img id="" src="" class="image">
+        <h3 id="price" class="name">${data.desc}</h3>
+        </div>`
+    )
     } catch (error){
-      
+        DOMSelectors.parent.insertAdjacentHTML(
+            "beforeend",
+            `<div class='card'>
+            <h2 id="name" class="name">Not Found</h2>
+            </div>`
+        )
     }
-   
 }
-
 DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
     getData();
